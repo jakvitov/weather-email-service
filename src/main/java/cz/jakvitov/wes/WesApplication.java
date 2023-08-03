d9c4435dbf63d517d0fbc2a9a4be2d9b34e25aa1
Jakub Vítovec <jakvitov@gmail.com>
2023-08-03 11:24:28 +0200

    Initial commit

---
+++ src/main/java/cz/jakvitov/wes/WesApplication.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.wes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/jakvitov/wes/WesApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.wes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
